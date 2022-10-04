42bb1c08bfcde9bd4349b1f3e80d3996089c9b47
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-10-04 10:12:23 -0300

    Add itemList components

---
+++ src/components/ItemList/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+// eslint-disable-next-line react/prop-types
+export default function ItemList({ title, description }) {
+  return (
+    <div className="item-list">
+      <strong>{title}</strong>
+      <p>{description}</p>
+      <hr />
+    </div>
+  );
+}
--- src/pages/Home/App.js
+++ src/pages/Home/App.js
@@ -1,5 +1,6 @@
 import { Header } from '../../components/Header';
 import background from '../../assets/background.png';
+import ItemList from '../../components/ItemList';
 
 import './styles.css';
 
@@ -27,6 +28,12 @@ function App() {
             </div>
           </div>
           <hr />
+          <div>
+            <h4 className="repositorio">Repositórios</h4>
+            <ItemList title="Teste1" description="Teste de descrição" />
+            <ItemList title="Teste1" description="Teste de descrição" />
+            <ItemList title="Teste1" description="Teste de descrição" />
+          </div>
         </div>
       </div>
     </div>
