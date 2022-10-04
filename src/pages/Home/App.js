5a9edbbd189a049ecec2c8c62fc884e317527df5
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-10-03 23:26:59 -0300

    Add profile in App components

--- src/pages/Home/App.js
+++ src/pages/Home/App.js
@@ -14,6 +14,19 @@ function App() {
             <input name="usuario" placeholder="@username" className="input" />
             <button className="button">Buscar</button>
           </div>
+          <div className="perfil">
+            <img
+              src="https://avatars.githubusercontent.com/u/75143575?v=4"
+              className="profile"
+              alt="imagem de perfil"
+            />
+            <div>
+              <h3>Lázaro Alves</h3>
+              <span>@Lazaro84-alv</span>
+              <p>Descrição</p>
+            </div>
+          </div>
+          <hr />
         </div>
       </div>
     </div>
