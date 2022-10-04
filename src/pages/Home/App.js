1e73a6f074e58cb78fa90153e27551a639335bb9
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-10-03 23:01:52 -0300

    Add button in App components

--- src/pages/Home/App.js
+++ src/pages/Home/App.js
@@ -1,9 +1,21 @@
 import { Header } from '../../components/Header';
+import background from '../../assets/background.png';
+
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="conteudo">
+        <img src={background} className="background" alt="" />
+        <div className="info">
+          <div>
+            <input name="usuario" placeholder="@username" className="input" />
+            <button className="button">Buscar</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
